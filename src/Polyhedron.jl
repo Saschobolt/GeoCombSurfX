9292639612cad249e286946c6254ea5d0d0cfcbc
Sascha Stüttgen <sascha.stuettgen@rwth-aachen.de>
2023-03-02 10:25:49 +0100

    Dependency angepasst

--- src/Polyhedron.jl
+++ src/Polyhedron.jl
@@ -1,5 +1,3 @@
-using GeometryBasics
-
 struct Polyhedron
     verts::Array{1, Array{1, Float32}} # vertex array. Every vertex is an array of 3 spatial coordinates
     edges::Array{1, Tuple{Int}} # edge array. Every edge is an array of the indices of the adjacent vertices
