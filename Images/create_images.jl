c35d6c51b836e5e808389fd7bac6ca7e0df93244
Sascha Stüttgen <sascha.stuettgen@rwth-aachen.de>
2023-08-28 06:21:15 +0200

    Plots für versch. Interlockings / Edge Edge Contacts

--- Images/create_images.jl
+++ Images/create_images.jl
@@ -24,6 +24,8 @@ function create_plots(width = 600, height = 600, color_frame = RGB(1,0,0), color
     p = plot(merge(nprism(6), nprism(8), [[1,2,7,8]], [[2,10,1,9]]), showbackground = showbackground, width = width, height = height, drawverts = false, opacity = opacity)
     PlotlyJS.savefig(p, "merge2.png", width = width, height = height)
 
+    @info "Small blocks figures saved."
+
     # 3 hat
     hat = nprism(3)
     merge!(hat, nprism(3), [[3,1,6,4]], [[1,2,4,5]])
@@ -44,37 +46,34 @@ function create_plots(width = 600, height = 600, color_frame = RGB(1,0,0), color
     p = plot(flattenfacets(tiblock(8,3,4)), showbackground = showbackground, width = width, height = height, drawverts = false, opacity = opacity)
     PlotlyJS.savefig(p, "block834.png", width = width, height = height)
 
+    @info "Blocks with 3hat figures saved."
+
     # assemblies with blocks above
     ass, frame = assembly1(6,3,2,7)
-    ass = flattenfacets.(ass)
     colors = [color_blocks for block in ass]
     colors[frame] .= color_frame
     p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
     PlotlyJS.savefig(p, "assembly1_6327.png", width = width, height = height)
 
     ass, frame = assembly1(8,3,2,7)
-    ass = flattenfacets.(ass)
     colors = [color_blocks for block in ass]
     colors[frame] .= color_frame
     p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
     PlotlyJS.savefig(p, "assembly1_8327.png", width = width, height = height)
 
-    ass, frame = assembly2(6,3,2,7)
-    ass = flattenfacets.(ass)
+    ass, frame = assembly2(6,3,2,7) 
     colors = [color_blocks for block in ass]
     colors[frame] .= color_frame
     p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
     PlotlyJS.savefig(p, "assembly2_6327.png", width = width, height = height)
 
     ass, frame = assembly2(8,3,2,7)
-    ass = flattenfacets.(ass)
     colors = [color_blocks for block in ass]
     colors[frame] .= color_frame
     p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
     PlotlyJS.savefig(p, "assembly2_8327.png", width = width, height = height)
 
     ass, frame, link = assembly3(6,4,3,1)
-    ass = flattenfacets.(ass)
     colors = [color_blocks for block in ass]
     colors[frame] .= color_frame
     colors[link] .= color_links
@@ -82,7 +81,6 @@ function create_plots(width = 600, height = 600, color_frame = RGB(1,0,0), color
     PlotlyJS.savefig(p, "assembly3_6431.png", width = width, height = height)
 
     ass, frame, link = assembly3(6,4,1,1)
-    ass = flattenfacets.(ass)
     colors = [color_blocks for block in ass]
     colors[frame] .= color_frame
     colors[link] .= color_links
@@ -90,18 +88,18 @@ function create_plots(width = 600, height = 600, color_frame = RGB(1,0,0), color
     PlotlyJS.savefig(p, "assembly3_6411.png", width = width, height = height)
 
     ass, frame = assembly1(6,3,3,7)
-    ass = flattenfacets.(ass)
     colors = [color_blocks for block in ass]
     colors[frame] .= color_frame
     p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
     PlotlyJS.savefig(p, "assembly1_6337.png", width = width, height = height)
 
     ass, frame = assembly1(8,3,4,7)
-    ass = flattenfacets.(ass)
     colors = [color_blocks for block in ass]
     colors[frame] .= color_frame
     p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
     PlotlyJS.savefig(p, "assembly1_8347.png", width = width, height = height)
+
+    @info "Assemblies with 3hat figures saved."
 
     # tiblocks with 4 hat
     p = plot(flattenfacets(tiblock(6,4,2)), showbackground = showbackground, width = width, height = height, drawverts = false, opacity = opacity)
@@ -122,46 +120,149 @@ function create_plots(width = 600, height = 600, color_frame = RGB(1,0,0), color
     p = plot(flattenfacets(tiblock(12,4,4)), showbackground = showbackground, width = width, height = height, drawverts = false, opacity = opacity)
     PlotlyJS.savefig(p, "block1244.png", width = width, height = height)
 
+    @info "Blocks with 4hat figures saved."
+
     # assemblies with blocks above
     ass, frame = assembly2(6,4,2,7)
-    ass = flattenfacets.(ass)
     colors = [color_blocks for block in ass]
     colors[frame] .= color_frame
     p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
     PlotlyJS.savefig(p, "assembly2_6427.png", width = width, height = height)
 
     ass, frame = assembly2(8,4,2,7)
-    ass = flattenfacets.(ass)
     colors = [color_blocks for block in ass]
     colors[frame] .= color_frame
     p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
     PlotlyJS.savefig(p, "assembly2_8427.png", width = width, height = height)
 
     ass, frame = assembly2(6,4,3,7)
-    ass = flattenfacets.(ass)
     colors = [color_blocks for block in ass]
     colors[frame] .= color_frame
     p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
     PlotlyJS.savefig(p, "assembly2_6437.png", width = width, height = height)
 
     ass, frame = assembly2(8,4,4,7)
-    ass = flattenfacets.(ass)
     colors = [color_blocks for block in ass]
     colors[frame] .= color_frame
     p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
     PlotlyJS.savefig(p, "assembly2_8447.png", width = width, height = height)
 
     ass, frame = assembly4(9,4,3,7)
-    ass = flattenfacets.(ass)
     colors = [color_blocks for block in ass]
     colors[frame] .= color_frame
     p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
     PlotlyJS.savefig(p, "assembly4_9437.png", width = width, height = height) 
 
     ass, frame = assembly4(12,4,4,7)
-    ass = flattenfacets.(ass)
     colors = [color_blocks for block in ass]
     colors[frame] .= color_frame
     p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
     PlotlyJS.savefig(p, "assembly4_12447.png", width = width, height = height) 
+
+    @info "Assemblies with 4hat figures saved."
+
+    # # Cube interlocking
+    # ass, frame = cube_interlocking(10)
+    # 
+    # colors = [color_blocks for block in ass]
+    # colors[frame] .= color_frame
+    # p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
+    # PlotlyJS.savefig(p, "cube_interlocking.png", width = width, height = height) 
+
+    # # Octahedra interlocking
+    # ass, frame = octahedra_interlocking(10)
+    # 
+    # colors = [color_blocks for block in ass]
+    # colors[frame] .= color_frame
+    # p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
+    # PlotlyJS.savefig(p, "octahedra_interlocking.png", width = width, height = height) 
+
+    # # Cube interlocking
+    # ass, frame = tetrahedra_interlocking(10)
+    # 
+    # colors = [color_blocks for block in ass]
+    # colors[frame] .= color_frame
+    # p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = opacity)
+    # PlotlyJS.savefig(p, "tetrahedra_interlocking.png", width = width, height = height)
+
+    # edge-face contact
+    block1 = deepcopy(Cube)
+    block2 = deepcopy(Cube)
+    aff = rigidmap(hcat([0,0,0], [1,0,0], [0,1,0], [0,0,1]), hcat([0.5,1,0], [0.5+1/sqrt(2), 1+1/sqrt(2), 0], [0.5-1/sqrt(2), 1+1/sqrt(2), 0], [0.5,1,1]))
+    set_verts!(block2, aff.(get_verts(block2)))
+    ass = [block1, block2]
+
+    colors = [color_blocks for block in ass]
+    p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = 0.55)
+    for i in 1:2, j in 1:2
+        add_trace!(
+            p,
+            scatter(
+                x = [0.5, 0.5],
+                y = [1, 1],
+                z = [0, 1],
+                line = attr(color = "red", width = 14),
+                mode = "lines",
+                type = "scatter3d"
+            ),
+            row = i, col = j
+        )
+    end
+
+    PlotlyJS.savefig(p, "contact_edge_facet.png", width = width, height = height)
+
+    # convex edge-edge contact
+    block1 = deepcopy(Cube)
+    block2 = deepcopy(Cube)
+    aff = rigidmap(hcat([0,0,0], [1,0,0], [0,1,0], [0,0,1]), hcat([1,1,0], [2,1,0], [1,2,0], [1,1,1]))
+    set_verts!(block2, aff.(get_verts(block2)))
+    ass = [block1, block2]
+
+    colors = [color_blocks for block in ass]
+    p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = 0.55)
+    for i in 1:2, j in 1:2
+        add_trace!(
+            p,
+            scatter(
+                x = [1, 1],
+                y = [1, 1],
+                z = [0, 1],
+                line = attr(color = "red", width = 14),
+                mode = "lines",
+                type = "scatter3d"
+            ),
+            row = i, col = j
+        )
+    end
+
+    PlotlyJS.savefig(p, "contact_convex_convex.png", width = width, height = height)
+
+    # mixed edge-edge contact
+    ass, frame = assembly1(6,3,2,2)
+    colors = [color_blocks for block in ass]
+    p = plot(ass, colors = colors, width = width, height = height, drawverts = false, showbackground = showbackground, opacity = 0.55)
+
+    edges = [[10,11], [7,8], [8,9], [11,12]]
+    block = ass[1]
+    for i in 1:2, j in 1:2
+        for e in edges
+            add_trace!(
+                p,
+                scatter(
+                    x = [get_verts(block)[e[1]][1], get_verts(block)[e[2]][1]],
+                    y = [get_verts(block)[e[1]][2], get_verts(block)[e[2]][2]],
+                    z = [get_verts(block)[e[1]][3], get_verts(block)[e[2]][3]],
+                    line = attr(color = "red", width = 14),
+                    mode = "lines",
+                    type = "scatter3d"
+                ),
+                row = i, col = j
+            )
+        end
+    end
+
+    PlotlyJS.savefig(p, "contact_convex_concave.png", width = width, height = height)
+
+    @info "Different contact points figures saved."
+    @info "Finished."
 end
