70e3b080702a3b0f437a937303d6d90e4898f2d6
Sascha Stüttgen <sascha.steuttgen@rwth-aachen.de>
2024-03-15 01:20:44 +0100

    renamed surrounding_sphere to bounding_sphere

--- src/affine_geometry.jl
+++ src/affine_geometry.jl
@@ -321,12 +321,12 @@ end
 
 Calculate the center and radius of a ball, in which all points are contained. Note, that this is not necessarily the smallest possible ball.
 """
-function surrounding_sphere(points::AbstractMatrix{<:Real})
+function bounding_sphere(points::AbstractMatrix{<:Real})
     center = center_of_mass(points)
     radius = maximum([dist(center, points[:,i]) for i in 1:size(points)[2]])
     return center, radius
 end
 
-function surrounding_sphere(points::Vector{<:Vector{<:Real}})
-    return surrounding_sphere(hcat(points...))
+function bounding_sphere(points::Vector{<:Vector{<:Real}})
+    return bounding_sphere(hcat(points...))
 end
